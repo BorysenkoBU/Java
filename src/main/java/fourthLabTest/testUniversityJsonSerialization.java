b7cbb1da88b3b83af5827a659e4c415872cf6dc3
BorysenkoBU <borisenko2001fr@gmail.com>
2023-11-29 13:43:41 +0200

    Lab_4: added testUniversityJsonSerialization

---
+++ src/main/java/fourthLabTest/testUniversityJsonSerialization.java
@@ -0,0 +1,22 @@
+import fourthLab.JsonManager;
+import fourthLab.controllers.UniversityCreator;
+import fourthLab.models.University;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testUniversityJsonSerialization {
+    @Test
+    public void testUniversityJsonSerialization() {
+        UniversityCreator universityCreate = new UniversityCreator();
+        JsonManager jsonManager = new JsonManager();
+        University oldUniversity = universityCreate.createTypicalUniversity();
+
+        jsonManager.writeToJsonFile("university.json", oldUniversity);
+
+
+        University newUniversity = jsonManager.readFromJsonFile("university.json");
+
+
+        Assert.assertEquals(oldUniversity, newUniversity);
+    }
+}
