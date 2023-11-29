67ee43ae196c64b50e2cd02992549a48a9117833
BorysenkoBU <borisenko2001fr@gmail.com>
2023-11-29 12:45:51 +0200

    Lab_3: create LR_3 branch, added models and controllers classes, added Runner

---
+++ src/main/java/thirdLab/Runner.java
@@ -0,0 +1,9 @@
+package thirdLab;
+
+import thirdLab.controller.UniversityCreator;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(UniversityCreator.createTypicalUniversity());
+    }
+}
